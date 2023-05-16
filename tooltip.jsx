9f7a91750cc066a2f61fad2e15522085b65ab9d0
Lulisderoo20 <92049842+Lulisderoo20@users.noreply.github.com>
2023-05-16 20:56:22 -0300

    Create tooltip.jsx

---
+++ tooltip.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Tooltip = ({ text, children }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
+  return (
+    <div className="tooltip-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {children}
+      {showTooltip && <div className="tooltip">{text}</div>}
+    </div>
+  );
+};
